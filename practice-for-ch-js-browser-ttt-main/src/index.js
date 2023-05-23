bdc58f10c2a1c7d93d48fff7221973850568c2dc
kennyvngo <kennyvungo@gmail.com>
2023-05-23 14:22:05 -0700

    rendered some squares

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,6 +1,15 @@
 // Add your import statements for View and Game here
 import View from "./ttt-view";
 import Game from "../ttt_node/game";
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  
+  const container = document.querySelector(".ttt");
+  const game = new Game();
+  const view = new View(game,container);
 });
+
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,8 +1,24 @@
 class View {
   constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard();
   }
   
   setupBoard() {
+    const board = document.createElement('ul');
+    board.className = 'board';
+
+    for(let row = 0; row < 3; row ++){
+      for(let col = 0; col < 3; col ++){
+        const cell = document.createElement("li");
+        cell.dataset.pos = [row,col];
+        board.appendChild(cell);
+      }
+      this.el.appendChild(board);
+    }
+
+
   }
   
   handleClick(e) {
